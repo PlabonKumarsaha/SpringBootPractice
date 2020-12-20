4bb7bb97f54b93aa5bdf305d33cbc2405c62f83a
Plabon Kumar saha <46686524+PlabonKumarsaha@users.noreply.github.com>
2020-12-20 19:01:39 +0600

    Add files via upload

--- ToDoRestApi/src/main/java/com/teksoi/restapi/repository/CategoryRepository.java
+++ ToDoRestApi/src/main/java/com/teksoi/restapi/repository/CategoryRepository.java
@@ -1,12 +1,24 @@
 package com.teksoi.restapi.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.teksoi.restapi.model.CategoryModel;
+import com.teksoi.restapi.model.ToDo;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<CategoryModel, Long>{
+	
+	@Modifying
+	@Query("update CategoryModel t set t.active = ?")
+	 int deleteAll(Boolean active);
+	
+	List<CategoryModel> findAllByActiveTrue();
+	
 
 	
 }
